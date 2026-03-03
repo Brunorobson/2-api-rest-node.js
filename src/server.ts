14cd62f310b860cb3e82395dd5183bdd8f24cb6d
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2026-03-03 17:42:58 -0300

    tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,19 @@
 import fastify = require('fastify')
 import crypto = require('crypto')
 import database = require('./database')
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transactions = await database
-    .knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transaction 1',
-      amount: 100,
-    })
-    .returning('*')
+  const transactions = await database.knex('transactions').select('*')
+
   return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
